c1e8df17870373f4e79a8ccfe534590b93e8eed4
Adomas Lebedys <adomas.lebedys@protonmail.com>
2022-01-15 18:16:46 +0000

    update helloworld test

--- helloworld/helloworld.ino
+++ helloworld/helloworld.ino
@@ -1,17 +1,23 @@
 const int baud     = 115200;
 const int delay_ms = 1000;
 
-const String msg   = "Hello, World!";
+const String msg = "Hello, World!";
+
+int i = 0;
 
 void setup() {
 
   Serial.begin(baud);
-  
+  while(!Serial); // Wait for serial monitor to connect
+
+  Serial.println("Setup Complete.");
+
 } 
 
 void loop() {
   
-  Serial.println(msg);
+  Serial.println(msg + " (loop " + i + ")");
   delay(delay_ms);
-  
+
+  i++;  
 } 
